4b9c17e33fc40891985dd592ec53e3897c716832
Alexis <Alexisesperanza0@gmail.com>
2023-10-01 12:38:50 -0600

    Cambio botones calculadora

--- app/src/main/java/com/example/operaciones/CalculadoraPolacaInversaApp.java
+++ app/src/main/java/com/example/operaciones/CalculadoraPolacaInversaApp.java
@@ -3,12 +3,25 @@ package com.example.operaciones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class CalculadoraPolacaInversaApp extends AppCompatActivity {
+    EditText edtPrimerN, edtSegunN;
+    RadioButton rdSuma, rdResta, rdMulti, rdDiv;
+    TextView tvRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora_polaca_inversa_app);
+        edtPrimerN = (EditText)findViewById(R.id.edtNumero1);
+        edtSegunN = (EditText)findViewById(R.id.edtNumero2);
+
+
+
     }
+
+
 }
